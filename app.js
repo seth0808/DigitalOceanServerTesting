6031e12f9d34306b95cbae4a00fcc5c9fc5ee915
Seth Kujawa <sethkujawa@gmail.com>
2018-11-03 22:15:50 -0400

    Update app.js

--- app.js
+++ app.js
@@ -34,6 +34,6 @@ app.get('/', function(req, res) {
 
 
 
-app.listen(22, function() {
-	console.log('Server started on port 22...')
+app.listen(3000, function() {
+	console.log('Server started on port 3000...')
 });
